1eba10342f1f39bc67ec5a1c3c84c0f4fe3e1bef
karagom2 <mert.karagoz@hva.nl>
2026-01-01 18:58:54 +0100

    Project toegevoegd

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,5 +16,13 @@ export const projects = [
     tech: ["React", "Vite"],
     liveUrl: "https://example.com",
     repoUrl: "https://github.com/jouwnaam/portfolio"
+  },
+  {
+    title: "Dashboard App",
+    description: "Frontend dashboard met filters, search en dummy data.",
+    tech: ["React", "CSS"],
+    liveUrl: "https://6956b456aaec2a17d650c8fb--fanciful-hotteok-24604b.netlify.app/",
+    repoUrl: "https://github.com/Mertoko2786/dashboard"
   }
+
 ];
